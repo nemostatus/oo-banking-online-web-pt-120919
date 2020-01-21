c7e502335ec27a8a8c05e8c826e080a8fd8f6b7f
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 13:58:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,9 +16,9 @@ class Transfer
   if @sender.balance > @amount && status == "pending" && @receiver.BankAccount.status == "open"
     @sender.balance -= @amount 
     @receiver.balance += @amount
-    self.status = "complete"
+    status = "complete"
 else 
- self.status == "rejected"
+ status == "rejected"
     return "Transaction rejected. Please check your account balance."
   end 
 end 
