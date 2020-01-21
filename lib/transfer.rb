5729dc621417cee5373aa35d4260562c5b9458a7
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 14:27:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,12 @@ class Transfer
       @sender.valid? && @receiver.valid? ? true:false
     end 
   def execute_transaction
-  if @sender.balance > @amount && @status == "pending" && @receiver.status == "open"
-    
+  if @sender.balance > @amount &&  self.status =="pending" && @receiver.status == "open"
     @sender.balance -= @amount 
     @receiver.balance += @amount
-    @status = "complete"
-else 
- @status == "rejected"
+@status = "complete"
+else
+@status = "rejected"
     return "Transaction rejected. Please check your account balance."
   end 
 end 
@@ -29,6 +28,4 @@ def reverse_transfer
     @receiver.balance -= @amount
     @status = "reversed"
   end 
-end 
- 
 end 
