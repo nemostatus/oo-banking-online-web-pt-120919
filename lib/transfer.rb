8112a8544e4f5d13312388d898543e7d21ea26e3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 13:49:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,8 @@ class Transfer
     @sender.balance -= @amount 
     @receiver.balance += @amount
     @status = "complete"
-  else
+  elsif
+  @self.new.!valid
  @status == "rejected"
     return "Transaction rejected. Please check your account balance."
   end 
