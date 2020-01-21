7bc71bd0c1e0a499d866cc7a10f841f6df5e2736
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 13:18:18 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,7 +6,7 @@ def initialize(account=nil,name)
   @account = account
    @name = name 
  @balance = 1000
- @status = "open"
+ @bank_status = "open"
   end
   def deposit(deposit)
   @balance = @balance + deposit
@@ -18,6 +18,6 @@ def initialize(account=nil,name)
    @status == "open" && @balance> 0 ? true:false 
  end 
  def close_account
-   @status = "closed"
+   @bank_status = "closed"
 end 
 end 
