ab0081e5c822db5a4f8dd8561235350cec08ae25
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 12:54:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
-class BankAccount
 
-end
+class BankAccount
+  attr_accessor :balance,:status 
+  attr_reader :name 
+def initialize(account=nil,name)
+  @account = account
+   @name = name 
+ @balance = 1000
+ @status = "open"
+  end
+  def deposit(deposit)
+  @balance = @balance + deposit
+ end 
+ def display_balance
+   "Your balance is $#{self.balance}."
+ end 
+ def valid?
+   @status == "open" && @balance> 0 ? true:false 
+ end 
+ def close_account
+   @status = "closed"
+end 
+end 
