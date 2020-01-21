6b4c3f26d04a0316f31a3266663cae9199cd8dd4
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 13:23:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,23 +1,23 @@
 
 class Transfer
 
-  attr_accessor :sender,:receiver,:amount,:status
+  attr_accessor :sender,:receiver,:amount,:Status
   def initialize(sender,receiver,amount)
     @sender = sender 
     @receiver = receiver 
     @amount = amount
-    @status = "pending"
+    @Status = "pending"
   end 
     def valid?
       @sender.valid? && @receiver.valid? ? true:false
     end 
   def execute_transaction
-  if @sender.balance > @amount && @status == "pending" && @receiver.BankAccount.status== "open"
+  if @sender.balance > @amount && @Status == "pending" && @receiver.status== "open"
     @sender.balance -= @amount 
     @receiver.balance += @amount
-    @status = "complete"
+    @Status = "complete"
   else
- @status == "rejected"
+ @Status == "rejected"
     return "Transaction rejected. Please check your account balance."
   end 
 end 
